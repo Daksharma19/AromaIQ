b6dfb5e2b104241eeaa653340d56af35b52eb735
Daksharma19 <shyper73@gmail.com>
2025-07-30 13:56:20 +0530

    hide the web token

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://zczurrdzcanzihkaumlj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjenVycmR6Y2Fuemloa2F1bWxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4NjE4MjksImV4cCI6MjA2OTQzNzgyOX0.Qmre0hGRuZwWh5bT2IW3WLIzKOYAOds0EPPdeaJlWk0';
+// const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjenVycmR6Y2Fuemloa2F1bWxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4NjE4MjksImV4cCI6MjA2OTQzNzgyOX0.Qmre0hGRuZwWh5bT2IW3WLIzKOYAOds0EPPdeaJlWk0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
